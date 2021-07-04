58b224920572509037835e26a1d7faf58cb2a33d
Eric Wilson <84479785+wilsoneg3@users.noreply.github.com>
2021-07-04 18:31:11 +0000

    Update the date prog

--- pre/2/Date.java
+++ pre/2/Date.java
@@ -4,26 +4,29 @@ import java.util.*;
 public class Date {
   public static void main(String[] args) {
   
-  
   String day = "Friday";
   String month = "June";
   int date = 18;
   int year = 2021;
-  System.out.print("The current date is ");
+ 
+  System.out.println("American Format:");
   System.out.print(day);
-  System.out.print(",");
+  System.out.print(", ");
   System.out.print(month);
+  System.out.print(", ");
   System.out.print(date);
-  System.out.print(",");
+  System.out.print(", ");
   System.out.print(year);
   System.out.println(".");
+  System.out.println("European Format:");
   System.out.print(day);
-  System.out.print("  ");
+  System.out.print(", ");
   System.out.print(date);
   System.out.print("  ");
   System.out.print(month);
   System.out.print("  ");
-  System.out.print(year);  
-  System.out.println(".");  
+  System.out.print(year);
+
+
   }
 }
